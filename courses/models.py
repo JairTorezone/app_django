3d8c8e663c2c4d6cdd1d123aef4ef45edea7e592
Jair Torezone <jairtorezone@gmail.com>
2025-04-21 19:46:55 -0300

    feat: Add image field to Course model

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 class Course(models.Model):
     """Modelo que representa um curso"""
@@ -9,14 +10,39 @@ class Course(models.Model):
         verbose_name="Preço Original",
         blank=True,
         null=True)
+
     discounted_price = models.DecimalField(
         max_digits=10, 
         decimal_places=2, 
         verbose_name="Preço Desconto",
         blank=True,
         null=True)
+
+    image = models.ImageField(
+        upload_to="courses/", 
+        verbose_name="Imagem",
+        blank=True,
+        null=True)
+
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de criação")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Data de Edição")
+
+    #Sobrescrever o Método save no modelo
+    def save(self, *args, **kwargs):
+        if self.pk:
+            old_image = Course.objects.get(pk=self.pk).image
+
+            if old_image and old_image != self.image:
+                if os.path.isfile(old_image.path):
+                    os.remove(old_image.path)
+
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.image and os.path.isfile(self.image.path):
+            os.remove(self.image.path)
+
+        super().delete(*args, **kwargs)
 
     def __str__(self):
         """Retorna str: O nome do curso"""
